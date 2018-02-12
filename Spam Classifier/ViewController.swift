f70b020c9c9c89d30652b386081af6f2dfafda02
Rudra Jikadra <rhjikadra@gmail.com>
2018-02-12 21:41:42 +0530

    Initial Commit

---
+++ Spam Classifier/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spam Classifier
+//
+//  Created by Rudra Jikadra on 12/02/18.
+//  Copyright © 2018 RedRudy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
